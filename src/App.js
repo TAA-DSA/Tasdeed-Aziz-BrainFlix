de11c5d0da4658453809f9a11f8bfc0fc0aca7f1
Tasdeed Aziz <ayonaziz@gmail.com>
2021-08-19 03:22:39 -0400

    comment component update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Video from './components/Video';
 import video from './data/videos.json';
 import Bio from './components/Bio';
+import Comment from './components/Comment';
 import './App.scss';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Header />
     <Video video={video[0]} />
     <Bio />
+    <Comment />
    </>
     
   );
---
+++ src/components/comment.js
@@ -0,0 +1,25 @@
+import userImg from '../assets/Images/Mohan-muruge.jpg';
+const number = 3
+const header = 'JOIN THE CONVERSATION'
+
+export default function Comment(){
+    return(
+        <section className='comment__section'>
+            <div>
+                <h2>{number} Comments</h2>
+                <div className='comForm__container'>
+                <img className='user__img--mohan' src={userImg} alt='user' />
+                <div>
+                <h2 className='comment__header'>{header}</h2>
+                <textarea className='form__input' placeholder= 'Write a comment here' /> 
+                <div className='commentBtn'>
+                    <button className='comment__submit'>COMMENT</button>
+                </div>
+                </div>
+                
+                
+                </div>
+            </div>
+        </section>
+    )
+}
