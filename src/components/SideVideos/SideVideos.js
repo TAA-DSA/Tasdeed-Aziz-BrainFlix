b4282d1d3d7dd993c4ba30d1d60c76c7640d0923
Tasdeed Aziz <ayonaziz@gmail.com>
2021-08-24 14:06:14 -0400

    styling update

--- src/components/SideVideos/SideVideos.js
+++ src/components/SideVideos/SideVideos.js
@@ -1,35 +1,35 @@
-export default function SideVideos(props){
-    
-    //console.log(props)
-    return(
-        
-        <section className='sidevideo__list'>
-            <h4 className='sidevideo--header'>Next Video</h4>
-               {props.videoList.map((thumbnail)=>{
-                if(thumbnail.title !== props.videoList[0].title){
-                    return (
-                        <div key={thumbnail.id} className='sidevideo'
-                        onClick={() => props.handleClick(thumbnail.id)}>
-                        <div className='sidevideo__data'>
-                        <img className='video__thumbnail' src={thumbnail.image} alt='side videos'></img>
-                        <div className='video__details'>
-                            <ul>
-                                <li className='sidevideo--title'>{thumbnail.title}</li>
-                                <li className='sidevideo--channel'>{thumbnail.channel}</li>
-                            </ul>
-                        </div>
-                        </div>
-                        </div>
-                        )   
-                }else{
-                    return false;
-                }
-                    
-                
-                
-                })}
-
-            </section>    
-              
-    )
+export default function SideVideos(props) {
+  //console.log(props)
+  return (
+    <section className="sidevideo__list">
+      <h4 className="sidevideo--header">Next Video</h4>
+      {props.videoList.map((thumbnail) => {
+        if (thumbnail.title !== props.videoList[0].title) {
+          return (
+            <div
+              key={thumbnail.id}
+              className="sidevideo"
+              onClick={() => props.handleClick(thumbnail.id)}
+            >
+              <div className="sidevideo__data">
+                <img
+                  className="video__thumbnail"
+                  src={thumbnail.image}
+                  alt="side videos"
+                ></img>
+                <div className="video__details">
+                  <ul>
+                    <li className="sidevideo--title">{thumbnail.title}</li>
+                    <li className="sidevideo--channel">{thumbnail.channel}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          );
+        } else {
+          return false;
+        }
+      })}
+    </section>
+  );
 }
