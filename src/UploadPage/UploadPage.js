f27f6b222c42367a2ca43a49ce4df686d7fcbb91
Tasdeed Aziz <ayonaziz@gmail.com>
2021-08-29 10:55:11 -0400

    upload page asthetic fixed

--- src/UploadPage/UploadPage.js
+++ src/UploadPage/UploadPage.js
@@ -39,50 +39,53 @@ class UploadPage extends Component {
         <h1 className="upload__header">Upload Video</h1>
         <div className="upload__container">
           <h3 className="upload__subheader">VIDEO THUMBNAIL</h3>
-          <img
-            className="upload__img"
-            src={uploadVideo}
-            alt="blue cycle handle"
-          />
-          <div>
-            <form onSubmit={this.handleSubmit} className="upload__form">
-              <div>
-                <h3 className="upload__form__title">TITLE YOUR VIDEO</h3>
-                <textarea
-                  type="text"
-                  name="title"
-                  onChange={this.handleChange}
-                  value={this.state.title}
-                  className="upload__form__titlebox"
-                  placeholder="Add a title to your video"
-                />
-              </div>
-              <div>
-                <h3 className="upload__form__description">
-                  ADD A VIDEO DESCRIPTION
-                </h3>
-                <textarea
-                  type="text"
-                  name="description"
-                  onChange={this.handleChange}
-                  value={this.state.description}
-                  className="upload__form__descriptionbox"
-                  placeholder="Add a description of your video"
-                />
-              </div>
-              <div className="upload__button">
-                <input
-                  type="submit"
-                  className="upload__button__publish"
-                  value="PUBLISH"
-                />
-                <input
-                  type="submit"
-                  className="upload__button__cancel"
-                  value="CANCEL"
-                />
-              </div>
-            </form>
+          <div className="upload__dataconatainer">
+            <img
+              className="upload__img"
+              src={uploadVideo}
+              alt="blue cycle handle"
+            />
+            <div>
+              <form onSubmit={this.handleSubmit} className="upload__form">
+                <div>
+                  <h3 className="upload__form__title">TITLE YOUR VIDEO</h3>
+                  <textarea
+                    type="text"
+                    name="title"
+                    onChange={this.handleChange}
+                    value={this.state.title}
+                    className="upload__form__titlebox"
+                    placeholder="Add a title to your video"
+                  />
+                </div>
+                <div>
+                  <h3 className="upload__form__description">
+                    ADD A VIDEO DESCRIPTION
+                  </h3>
+                  <textarea
+                    type="text"
+                    name="description"
+                    onChange={this.handleChange}
+                    value={this.state.description}
+                    className="upload__form__descriptionbox"
+                    placeholder="Add a description of your video"
+                  />
+                </div>
+
+                <div className="upload__button">
+                  <input
+                    type="submit"
+                    className="upload__button__publish"
+                    value="PUBLISH"
+                  />
+                  <input
+                    type="submit"
+                    className="upload__button__cancel"
+                    value="CANCEL"
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </main>
